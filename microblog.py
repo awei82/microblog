70bf823f58ac5772179729a8f81d6e4a737e3418
Andrew Wei <awei34@gatech.edu>
2020-11-29 09:01:55 -0800

    remove dubugging code

--- microblog.py
+++ microblog.py
@@ -7,11 +7,6 @@ from dotenv import load_dotenv
 dotenv_path = join(dirname(__file__), '.flaskenv')
 load_dotenv(dotenv_path)
 
-print('config:')
-print(app.config['MAIL_USE_SSL'])
-print(app.config['SECURITY_EMAIL_SENDER'])
-print(app.config['MAIL_USERNAME'])
-
 @app.shell_context_processor
 def make_shell_context():
     return {'db': db, 'User': User, 'Post': Post}
