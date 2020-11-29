5a599c61860ab348712c74ce4fe225c49a59f33a
Andrew Wei <awei34@gatech.edu>
2020-11-28 23:09:52 -0800

    ch.10 - emails - not tested

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+from flask_mail import Mail
 
 import logging
 from logging.handlers import SMTPHandler, RotatingFileHandler
@@ -18,6 +19,7 @@ migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
 
+mail = Mail(app)
 
 from app import routes, models, errors
 
