542f49a19a0b854bd4890d0f229fcf39ac5bd13e
Andrew Wei <awei34@gatech.edu>
2020-11-29 10:15:00 -0800

    ch.11 bootstrap css

--- app/email.py
+++ app/email.py
@@ -14,9 +14,8 @@ def send_email(subject, sender, recipients, text_body, html_body):
     msg = Message(subject, sender=sender, recipients=recipients)
     msg.body = text_body
     msg.html = html_body
-    #Thread(target=send_async_email, args=(app, msg)).start()
-
-    mail.send(msg)
+    Thread(target=send_async_email, args=(app, msg)).start()
+    #mail.send(msg)
 
 
 def send_password_reset_email(user):
